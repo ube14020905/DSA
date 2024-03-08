62a9740ea4987212c8cd5872f60108c2a5237660
UBEDULLA SHAIK <97379715+ube14020905@users.noreply.github.com>
2024-03-08 17:28:39 +0530

    update queue.h

--- Linear DS/Queue/Queue.h
+++ Linear DS/Queue/Queue.h
@@ -1,3 +1,15 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+template<typename T>
+class Queue{
+    private:
+        int size;
+        int count;
+        T* queue;
+    public:
+        Queue(int size);
+        void enQue(T data);
+        void deQue();
+        int getCount();
+}
 #endif
