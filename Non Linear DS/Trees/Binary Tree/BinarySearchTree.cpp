ac845c59a0e00ba5444276d03ab055494e064bfc
Ube140209 <ubedullashaik11@gmail.com>
2024-03-12 12:34:47 +0530

    Format change in BST.cpp

--- Non Linear DS/Trees/Binary Tree/BinarySearchTree.cpp
+++ Non Linear DS/Trees/Binary Tree/BinarySearchTree.cpp
@@ -3,68 +3,93 @@
 
 using namespace std;
 
-template<typename T>
+template <typename T>
 BST<T>::BST() : root(nullptr) {}
 
-template<typename T>
-void BST<T>::insert(T data) {
+template <typename T>
+void BST<T>::insert(T data)
+{
     root = insertRecursive(root, data);
 }
 
-template<typename T>
-Node<T>* insertRecursive(Node<T>* node, T data) {
-    if (node == nullptr) {
+template <typename T>
+Node<T> *insertRecursive(Node<T> *node, T data)
+{
+    if (node == nullptr)
+    {
         node = new Node<T>;
         node->data = data;
         node->left = node->right = nullptr;
-    } else if (data < node->data) {
+    }
+    else if (data < node->data)
+    {
         node->left = insertRecursive(node->left, data);
-    } else if (data > node->data) {
+    }
+    else if (data > node->data)
+    {
         node->right = insertRecursive(node->right, data);
     }
     return node;
 }
 
-template<typename T>
-bool BST<T>::search(T key) {
+template <typename T>
+bool BST<T>::search(T key)
+{
     return searchRecursive(root, key);
 }
 
-template<typename T>
-bool searchRecursive(Node<T>* node, T key) {
-    if (node == nullptr) {
+template <typename T>
+bool searchRecursive(Node<T> *node, T key)
+{
+    if (node == nullptr)
+    {
         return false;
     }
 
-    if (key == node->data) {
+    if (key == node->data)
+    {
         return true;
-    } else if (key < node->data) {
+    }
+    else if (key < node->data)
+    {
         return searchRecursive(node->left, key);
-    } else {
+    }
+    else
+    {
         return searchRecursive(node->right, key);
     }
 }
 
-template<typename T>
-void BST<T>::remove(T key) {
+template <typename T>
+void BST<T>::remove(T key)
+{
     root = removeRecursive(root, key);
 }
 
-template<typename T>
-Node<T>* removeRecursive(Node<T>* node, T key) {
-    if (node == nullptr) {
+template <typename T>
+Node<T> *removeRecursive(Node<T> *node, T key)
+{
+    if (node == nullptr)
+    {
         return nullptr;
     }
 
-    if (key < node->data) {
+    if (key < node->data)
+    {
         node->left = removeRecursive(node->left, key);
-    } else if (key > node->data) {
+    }
+    else if (key > node->data)
+    {
         node->right = removeRecursive(node->right, key);
-    } else {
-        if (node->left == nullptr) return node->right;
-        else if (node->right == nullptr) return node->left;
+    }
+    else
+    {
+        if (node->left == nullptr)
+            return node->right;
+        else if (node->right == nullptr)
+            return node->left;
 
-        Node<T>* temp = findMinNode(node->right);
+        Node<T> *temp = findMinNode(node->right);
         node->data = temp->data;
         node->right = removeRecursive(node->right, temp->data);
     }
@@ -72,88 +97,105 @@ Node<T>* removeRecursive(Node<T>* node, T key) {
     return node;
 }
 
-
-template<typename T>
-Node<T>* findMinNode(Node<T>* node) {
-    while (node->left != nullptr) {
+template <typename T>
+Node<T> *findMinNode(Node<T> *node)
+{
+    while (node->left != nullptr)
+    {
         node = node->left;
     }
     return node;
 }
 
-template<typename T>
-void BST<T>::inorderTraversal() {
+template <typename T>
+void BST<T>::inorderTraversal()
+{
     inorderRecursive(root);
     cout << endl;
 }
 
-template<typename T>
-void inorderRecursive(Node<T>* node) {
-    if (node != nullptr) {
+template <typename T>
+void inorderRecursive(Node<T> *node)
+{
+    if (node != nullptr)
+    {
         inorderRecursive(node->left);
         cout << node->data << " ";
         inorderRecursive(node->right);
     }
 }
 
-template<typename T>
-void BST<T>::preorderTraversal() {
+template <typename T>
+void BST<T>::preorderTraversal()
+{
     preorderRecursive(root);
     cout << endl;
 }
 
-template<typename T>
-void preorderRecursive(Node<T>* node) {
-    if (node != nullptr) {
+template <typename T>
+void preorderRecursive(Node<T> *node)
+{
+    if (node != nullptr)
+    {
         cout << node->data << " ";
         preorderRecursive(node->left);
         preorderRecursive(node->right);
     }
 }
 
-template<typename T>
-void BST<T>::postorderTraversal() {
+template <typename T>
+void BST<T>::postorderTraversal()
+{
     postorderRecursive(root);
     cout << endl;
 }
 
-template<typename T>
-void postorderRecursive(Node<T>* node) {
-    if (node != nullptr) {
+template <typename T>
+void postorderRecursive(Node<T> *node)
+{
+    if (node != nullptr)
+    {
         postorderRecursive(node->left);
         postorderRecursive(node->right);
         cout << node->data << " ";
     }
 }
 
-template<typename T>
-int BST<T>::findMin() {
-    Node<T>* minNode = findMinNode(root);
+template <typename T>
+int BST<T>::findMin()
+{
+    Node<T> *minNode = findMinNode(root);
     return minNode != nullptr ? minNode->data : -1;
 }
 
-template<typename T>
-int BST<T>::findMax() {
-    Node<T>* maxNode = findMaxNode(root);
+template <typename T>
+int BST<T>::findMax()
+{
+    Node<T> *maxNode = findMaxNode(root);
     return maxNode != nullptr ? maxNode->data : -1;
-    }
+}
 
-template<typename T>
-Node<T>* findMaxNode(Node<T>* node) {
-    while (node->right != nullptr) {
+template <typename T>
+Node<T> *findMaxNode(Node<T> *node)
+{
+    while (node->right != nullptr)
+    {
         node = node->right;
     }
     return node;
 }
 
-template<typename T>
-int BST<T>::getHeight() {
+template <typename T>
+int BST<T>::getHeight()
+{
     return calculateHeight(root);
 }
 
-template<typename T>
-int calculateHeight(Node<T>* node) {
-    if (node == nullptr) {
+template <typename T>
+int calculateHeight(Node<T> *node)
+{
+    if (node == nullptr)
+    {
         return 0;
     }
 
@@ -163,14 +205,17 @@ int calculateHeight(Node<T>* node) {
     return 1 + max(leftHeight, rightHeight);
 }
 
-template<typename T>
-BST<T>::~BST() {
+template <typename T>
+BST<T>::~BST()
+{
     destroyTree(root);
 }
 
-template<typename T>
-void destroyTree(Node<T>* node) {
-    if (node != nullptr) {
+template <typename T>
+void destroyTree(Node<T> *node)
+{
+    if (node != nullptr)
+    {
         destroyTree(node->left);
         destroyTree(node->right);
         delete node;
