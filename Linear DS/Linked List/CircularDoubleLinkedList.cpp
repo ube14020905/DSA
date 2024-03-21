d6487e41c3f9313c6bf0d6a3ee3105614ed5c816
Ube140209 <ubedullashaik11@gmail.com>
2024-03-21 10:59:00 +0530

    update circulardoublylinked list

--- Linear DS/Linked List/CircularDoubleLinkedList.cpp
+++ Linear DS/Linked List/CircularDoubleLinkedList.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 template <class T>
@@ -10,13 +10,13 @@ void DoubleCircularLinkedList<T>::displayFromBack()
         return;
     }
 
-    DLLNode<T> *current = this->getHead()->prev;
+    DLLNode<T> *current = head->prev;
     cout << "Data in Double Circular Linked List (from back): ";
     do
     {
         cout << current->data << "\t";
         current = current->prev;
-    } while (current != this->getHead()->prev);
+    } while (current != head->prev);
     cout << endl;
 }
 
@@ -30,17 +30,17 @@ void DoubleCircularLinkedList<T>::append(T data)
 
     if (this->isEmpty())
     {
-        this->setHead(newNode);
+        head = newNode;
         newNode->next = newNode;
         newNode->prev = newNode;
     }
     else
     {
-        DLLNode<T> *current = this->getHead()->prev;
+        DLLNode<T> *current = head->prev;
         current->next = newNode;
         newNode->prev = current;
-        newNode->next = this->getHead();
-        this->getHead()->prev = newNode;
+        newNode->next = head;
+        head->prev = newNode;
     }
     this->incrementCount();
 }
@@ -61,15 +61,15 @@ void DoubleCircularLinkedList<T>::insert(T data, int pos)
 
     if (pos == 0)
     {
-        newNode->next = this->getHead();
-        newNode->prev = this->getHead()->prev;
-        this->getHead()->prev->next = newNode;
-        this->getHead()->prev = newNode;
-        this->setHead(newNode);
+        newNode->next = head;
+        newNode->prev = head->prev;
+        head->prev->next = newNode;
+        head->prev = newNode;
+        head = newNode;
     }
     else
     {
-        DLLNode<T> *current = this->getHead()->prev;
+        DLLNode<T> *current = head->prev;
         for (int i = 0; i < pos - 1; ++i)
         {
             current = current->prev;
@@ -93,16 +93,16 @@ void DoubleCircularLinkedList<T>::remove(int pos)
 
     if (pos == 0)
     {
-        DLLNode<T> *temp = this->getHead();
-        this->getHead()->prev->next = this->getHead()->next;
-        this->getHead()->next->prev = this->getHead()->prev;
-        this->setHead(this->getHead()->next);
+        DLLNode<T> *temp = head;
+        head->prev->next = head->next;
+        head->next->prev = head->prev;
+        head = head->next;
 
         delete temp;
     }
     else
     {
-        DLLNode<T> *current = this->getHead()->prev;
+        DLLNode<T> *current = head->prev;
         for (int i = 0; i < pos - 1; ++i)
         {
             current = current->prev;
@@ -116,4 +116,3 @@ void DoubleCircularLinkedList<T>::remove(int pos)
     }
     this->decrementCount();
 }
-
